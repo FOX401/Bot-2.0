b2ed4f89d05494b61ef8c490c654de1b48762ed7
FOX401 <76862424+FOX401@users.noreply.github.com>
2021-02-19 16:00:05 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5521979664189 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*CHAT DOS BOTS 🚩 :*
+_https://chat.whatsapp.com/F2QW6H6gzmrEztMZfpxKrI_ `
+}
+exports.daftarvip = daftarvip
